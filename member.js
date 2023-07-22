c85c10454f8884f8a108fa405c11e23081572531
hindkh21 <99983177+hindkh21@users.noreply.github.com>
2023-07-22 00:19:14 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,44 @@
+function skillsMember()
+{
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var skillsMember = document.getElementById("skillsMember");
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsMember = document.getElementById("memberSkillsMember");
+    var memberMember = document.getElementById("memberMember");
+    var skillsSkills = document.getElementById("skillsSkills");
+    var skillsSkillsSkills = document.getElementById("skillsSkillsSkills");
+    var skillsMemberSkills = document.getElementById("skillsMemberSkills");
+    var memberMemberMember = document.getElementById("memberMemberMember");
+    var memberSkillsSkills = document.getElementById("memberSkillsSkills");
+    var memberMemberSkills = document.getElementById("memberMemberSkills");
+    var skillsSkillsMember = document.getElementById("skillsSkillsMember");
+    var skillsMemberMember = document.getElementById("skillsMemberMember");
+    var memberSkillsMember = document.getElementById("memberSkillsMember");
+    var memberMemberMember = document.getElementById("memberMemberMember");
+    var memberMemberSkills = document.getElementById("memberMemberSkills");
+    var skillsSkillsSkills = document.getElementById("skillsSkillsSkills");
+    var skillsSkillsMember = document.getElementById("skillsSkillsMember");
+    var skillsMemberSkills = document.getElementById("skillsMemberSkills");
+    var memberSkillsSkills = document.getElementById("memberSkillsSkills");
+    var memberMemberSkills = document.getElementById("memberMemberSkills");
+    var memberMemberMember = document.getElementById("memberMemberMember");
+    var memberSkillsMember = document.getElementById("memberSkillsMember");
+    var skillsSkillsMember = document.getElementById("skillsSkillsMember");
+    var skillsMemberMember = document.getElementById("skillsMemberMember");
+    var skillsSkillsSkills = document.getElementById("skillsSkillsSkills");
+    var skillsMemberSkills = document.getElementById("skillsMemberSkills");
+    var memberSkillsSkills = document.getElementById("memberSkillsSkills");
+    var memberMemberSkills = document.getElementById("memberMemberSkills");
+    var memberMemberMember = document.getElementById("memberMemberMember");
+    var memberSkillsMember = document.getElementById("memberSkillsMember");
+    var skillsSkillsMember = document.getElementById("skillsSkillsMember");
+    var skillsMemberMember = document.getElementById("skillsMemberMember");
+    var skillsSkillsSkills = document.getElementById("skillsSkillsSkills");
+    var skillsMemberSkills = document.getElementById("skillsMemberSkills");
+    var memberSkillsSkills = document.getElementById("memberSkillsSkills");
+    var memberMemberSkills = document.getElementById("memberMemberSkills");
+    var memberMemberMember = document.getElementById("memberMemberMember");
+    var memberSkillsMember = document.getElementById
+
+}
